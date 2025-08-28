e500aad930766550e5d258659551de0176b8ad28
Starboy <samskywalker001@gmail.com>
2025-08-28 03:27:20 +0000

    gsap.from()

--- src/app/gsap-from/page.tsx
+++ src/app/gsap-from/page.tsx
@@ -1,7 +1,53 @@
+"use client";
+
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
 export default function FromPage() {
+  useGSAP(() => {
+    gsap.from("#green-box", {
+      x: 250,
+      rotation: 360,
+      borderRadius: "100%",
+      duration: 2,
+      ease: "power1.inOut",
+      repeat: -1,
+      yoyo: true,
+    });
+  });
+
   return (
-    <div>
-      <h1>GSAP From</h1>
-    </div>
+    <main className="min-h-screen">
+      <h1 className="text-6xl">GSAP From</h1>
+
+      <p className="mt-5">
+        The <code>gsap.from()</code> method is used to animate elements from a
+        new state to their current state.
+      </p>
+
+      <p className="mt-5">
+        The <code>gsap.from()</code> method is similar to the{" "}
+        <code>gsap.to()</code> method, but the difference is that the{" "}
+        <code>gsap.from()</code> method animates elements from a new state to
+        their current state, while the <code>gsap.to()</code> method animates
+        elements from their current state to a new state.
+      </p>
+
+      <p className="mt-5">
+        Read more about the{" "}
+        <a
+          href="https://greensock.com/docs/v3/GSAP/gsap.from()"
+          target="_blank"
+          rel="noreferrer noopener nofollow"
+        >
+          gsap.from()
+        </a>{" "}
+        method.
+      </p>
+
+      <div className="mt-20">
+        <div id="green-box" className="h-20 w-20 rounded-lg bg-primary" />
+      </div>
+    </main>
   );
 }
